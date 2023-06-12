553d16f4d18ff9302cffa4e64993bb46eac61a19
Lalit_Kishor <lalitkishor202125@gmail.com>
2023-06-12 22:29:57 +0530

    added alert to component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 import TextForm from "./components/TextForms";
+// import React, { useState } from 'react';
+import Navbar from "./components/NavBar";
+import Alert from "./Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert=(message , type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+  const toggleMode=()=>{
+    if(mode==="dark"){
+    setMode("light");
+      document.body.style.backgroundColor="white";
+      showAlert("Light mode has been enabled" ,"success");
+    }
+    else{
+    setMode("dark");
+    document.body.style.backgroundColor="#042743";
+    showAlert("Dark mode has been enabled" ,"success");
+      
+    }
+  }
   return (
     <>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
       <div className="container">
-
-    <TextForm/>
+    <TextForm showAlert={showAlert} mode={mode}/>
       </div>
     </>
   );
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,37 +1,47 @@
 import React,{useState} from "react";
 export default function TextForm(props) {
-    const [text,setText]=useState("Here is your text");
+    const [text,setText]=useState("");
     const isEmpty=text.trim().length === 0;
     const wordCount = isEmpty ? 0 : text.split(" ").length;
     const convertUpCase=()=>{
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to upper case","success");
     }
     const convertLoCase=()=>{
       let newText=text.toLowerCase();
       setText(newText);
+      props.showAlert("Converted to lower case","success");
+
     }
     const handleOnChange=(event)=>{
       setText(event.target.value)
     }
     const clearText=()=>{
       setText("");
+      props.showAlert("Text cleared","success");
+
     }
     const copyText=()=>{
       let text=document.getElementById("myBox");
       // text.select();
       navigator.clipboard.writeText(text.value);
+      props.showAlert("Copied to clipboard","success");
+
     }
     const extraSpace=()=>{
       let newText=text.split(/[ ]+/);
       setText(newText.join(" "));
+      props.showAlert("Extra spaces rempved","success");
+
     }
   return (
     <>
     <div className="container">
+      <h1 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>Enter the text to analyse</h1>
       <div className="mb-3 ">
         <h1>{props.heading} </h1>
-        <textarea  className="form-control" onChange={handleOnChange} aria-label="With textarea" value={text} id="myBox" rows="8"></textarea>
+        <textarea  className="form-control" style={{backgroundColor:(props.mode==="light"?"white":"#042743"), color:(props.mode==="light"?"Black":"white")}} onChange={handleOnChange} aria-label="With textarea" value={text} id="myBox" rows="8"></textarea>
       </div>
       <div >
       <button className="mx-2 my-3 btn btn-primary" onClick={convertUpCase}>Convert to UpperCase</button>
@@ -42,11 +52,11 @@ export default function TextForm(props) {
       </div>
     </div>
     <div className="container my-2 ">
-      <h1 className="d-flex justify-content-center ">Your Text Summary</h1>
-      <h5 className="d-flex justify-content-center ">Words:    {wordCount}</h5>
-      <h5 className="d-flex justify-content-center ">Characters: {text.length}</h5>
-      <h2 >Preview</h2>
-      <p>{text}</p>
+      <h1 className={`d-flex justify-content-center text-${props.mode === 'light' ? 'dark' : 'light'}`}>Your Text Summary</h1>
+      <h5 className={`d-flex justify-content-center text-${props.mode === 'light' ? 'dark' : 'light'}`}>Words:    {wordCount}</h5>
+      <h5 className={`d-flex justify-content-center text-${props.mode === 'light' ? 'dark' : 'light'}`}>Characters: {text.length}</h5>
+      <h2 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>Preview</h2>
+      <p className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{text.length>0?text:"Enter something to preview it here"}</p>
     </div>
     
     </>
