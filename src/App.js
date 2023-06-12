52b18b7ff6c3b7ee347b92fd962fcd3ccd644372
Lalit_Kishor <lalitkishor202125@gmail.com>
2023-06-12 20:51:55 +0530

    Text Analyser

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TextForm from "./components/TextForms";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+
+    <TextForm/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForms.js
@@ -0,0 +1,39 @@
+import React,{useState} from "react";
+export default function TextForm(props) {
+    const [text,setText]=useState("Here is your text");
+    const isEmpty=text.trim().length === 0;
+    const wordCount = isEmpty ? 0 : text.split(" ").length;
+    const convertUpCase=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+    }
+    const convertLoCase=()=>{
+      let newText=text.toLowerCase();
+      setText(newText);
+    }
+    const handleOnChange=(event)=>{
+      setText(event.target.value)
+    }
+  return (
+    <>
+    <div className="container">
+      <div className="mb-3 ">
+        <h1>{props.heading} </h1>
+        <textarea  className="form-control" onChange={handleOnChange} aria-label="With textarea" value={text} id="myBox" rows="8"></textarea>
+      </div>
+      <div >
+      <button className="mx-2 my-3 btn btn-primary" onClick={convertUpCase}>Convert to UpperCase</button>
+      <button className="mx-2 my-3 btn btn-primary" onClick={convertLoCase}>Convert to LowerCase</button>
+      </div>
+    </div>
+    <div className="container my-2 ">
+      <h1 className="d-flex justify-content-center ">Your Text Summary</h1>
+      <h5 className="d-flex justify-content-center ">Words:    {wordCount}</h5>
+      <h5 className="d-flex justify-content-center ">Characters: {text.length}</h5>
+      <h2 >Preview</h2>
+      <p>{text}</p>
+    </div>
+    
+    </>
+  );
+}
